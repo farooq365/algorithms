add068c96f6e2151662ee1851b800dad07c6da91
farooq365 <91167693+farooq365@users.noreply.github.com>
2021-10-23 07:07:18 +0600

    binarySearch

---
+++ binarySearch.js
@@ -0,0 +1,21 @@
+function binarySearch(arr, target){
+    let start = 0;
+    let end = arr.length - 1;
+    while(end >= start){
+        let mid = Math.round((start + end)/2);
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(arr[mid] < target){
+            start = mid + 1;
+        }
+        else{
+            end = mid -1;
+        }
+    }
+    return -1;
+}
+
+
+const result = binarySearch([-10, -5, -2, 1, 2, 8, 11, 12], 11);
+console.log(result);
